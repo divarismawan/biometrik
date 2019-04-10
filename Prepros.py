3c4b1b3ac6208b099b229566aa7e3acfffa2d4a5
FX504 <divarismawan.20@gmail.com>
2019-04-10 18:41:37 +0800

    gausian dan laplasian

--- Prepros.py
+++ Prepros.py
@@ -16,10 +16,20 @@ for category in classes:
     path = os.path.join(url_class, category)
     print(category)
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path,img))
-        gray      = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        img_array = cv2.imread(os.path.join(path,img),0)
+
+        # gray      = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+        gaussian = cv2.GaussianBlur(img_array, (3, 3), 0)
+
+        lapl = cv2.Laplacian(gaussian, cv2.CV_16S, 3)
+
+        abs_dst = cv2.convertScaleAbs(lapl)
+
         cv2.imshow("asli",img_array)
-        cv2.imshow("hasil",gray)
+        cv2.imshow("hasil grayscale",gaussian)
+        cv2.imshow("hasil laplacian", abs_dst)
+
         cv2.waitKey(0)
         break
     break

b6259aebd6eb953abff4235a04e135aac6da2558
FX504 <divarismawan.20@gmail.com>
2019-04-17 17:40:17 +0800

    deteksi wajah

--- Prepros.py
+++ Prepros.py
@@ -1,9 +1,5 @@
 import os
 import cv2
-import matplotlib as plt
-
-
-
 
 url_class = 'face/'
 classes   = os.listdir(url_class)
@@ -12,24 +8,43 @@ print("Jumlah Kelas : {}".format(len(classes)))
 print("Nama Kelas : {}".format(classes))
 
 
+
 for category in classes:
     path = os.path.join(url_class, category)
     print(category)
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path,img),0)
+        img_array = cv2.imread('DSC05109.jpg')
 
-        # gray      = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        grey      = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-        gaussian = cv2.GaussianBlur(img_array, (3, 3), 0)
+        # Preprocessing
+        gaussian = cv2.GaussianBlur(grey, (3, 3), 0)
 
-        lapl = cv2.Laplacian(gaussian, cv2.CV_16S, 3)
+        lapl = cv2.Laplacian(gaussian, cv2.CV_16S, 2)
 
         abs_dst = cv2.convertScaleAbs(lapl)
 
-        cv2.imshow("asli",img_array)
+        # detected_faces = detect_faces(abs_dst)
+
+        cv2.imshow("grayscale",img_array)
         cv2.imshow("hasil grayscale",gaussian)
         cv2.imshow("hasil laplacian", abs_dst)
+        # cv2.imshow("hasil laplacian", detected_faces)
+
+
+        #Face Detection
+        face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+        faces = face_casecade.detectMultiScale(img_array,1.3, 1)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(grey, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray    = grey[y:y + h, x:x + w]
+            roi_color   = grey[y:y + h, x:x + w]
+
+        cv2.imshow('img', grey)
+
+        cv2.imshow('roi',roi_color)
 
         cv2.waitKey(0)
-        break
-    break
